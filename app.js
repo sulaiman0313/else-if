a8282bab0d12b703f7ea59f054397231d8d1c4e7
sulaiman <sulaiman@pop-os.localdomain>
2023-09-27 18:16:28 +0530

    else if

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+
+let randonm=Math.floor(Math.random() * 12)
+
+if(randonm>=0 && randonm<=4){
+    console.log(randonm  + " hi im  good morning")
+}
+else if(randonm>=5 && randonm<=10){
+    console.log(  12 - randonm + " minute left")
+}
+
+
+
+else{
+    console.log( randonm + ' you are late')
+}
