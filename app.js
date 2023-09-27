b8abb96a335048c6d07403af9298df77c6e1306a
sulaiman <sulaiman@pop-os.localdomain>
2023-09-27 18:27:58 +0530

    if else ludo

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 
 
 
-let randonm=Math.floor(Math.random() * 12)
+// let randonm=Math.floor(Math.random() * 12)
 
-if(randonm>=0 && randonm<=4){
-    console.log(randonm  + " hi im  good morning")
+// if(randonm>=0 && randonm<=4){
+//     console.log(randonm  + " hi im  good morning")
+// }
+// else if(randonm>=5 && randonm<=10){
+//     console.log(  12 - randonm + " minute left")
+// }
+
+
+
+// else{
+//     console.log( randonm + ' you are late')
+// }
+
+
+
+//ludo
+
+
+let dise=Math.floor(Math.random() * (6 - 1) + 1)
+
+if(dise==1){
+    console.log(dise)
 }
-else if(randonm>=5 && randonm<=10){
-    console.log(  12 - randonm + " minute left")
+else if(dise==2){
+    console.log(dise)
 }
-
-
-
+else if(dise==3){
+    console.log(dise)
+}
+else if(dise==4){
+    console.log(dise)
+}
+else if(dise==5){
+    console.log(dise)
+}
 else{
-    console.log( randonm + ' you are late')
+    console.log(dise)
 }
+
